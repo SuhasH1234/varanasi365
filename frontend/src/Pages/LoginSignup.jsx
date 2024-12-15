ba7ea4f76c361dd3ebe39dc187eb71aac90f1644
Suhas H <suhasharish9090@gmail.com>
2024-12-16 01:44:07 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login function executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/login', {
+    await fetch('https://varanasi365.onrender.com/login', {
       method: 'POST',
       headers:{
         Accept: 'application/form-data',
@@ -37,7 +37,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Signup function executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/signup', {
+    await fetch('https://varanasi365.onrender.com/signup', {
       method: 'POST',
       headers:{
         Accept: 'application/form-data',
