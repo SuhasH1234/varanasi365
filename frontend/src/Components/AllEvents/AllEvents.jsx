1bf43d23179345f1bcc2b1e3f086a2c1bd526cef
Suhas H <suhasharish9090@gmail.com>
2024-12-16 01:23:08 +0530

    Update AllEvents.jsx

--- frontend/src/Components/AllEvents/AllEvents.jsx
+++ frontend/src/Components/AllEvents/AllEvents.jsx
@@ -9,7 +9,7 @@ const AllEvents = () => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get('http://localhost:4000/allevents');
+        const response = await axios.get('https://varanasi365.onrender.com/allevents');
         setEvents(response.data);
       } catch (error) {
         console.error('Error fetching events:', error);
